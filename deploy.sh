48a4510e390051928c366f2acd798057c185816f
miguemendoza <125337855+miguejuarz@users.noreply.github.com>
2023-07-16 12:20:09 -0600

    add screenshot

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:Migue-Juarz/react-bettercallsaul-quotes-api.git main:gh-pages
+ git push -f git@github.com:miguejuarz/react-bettercallsaul-quotes.git main:gh-pages
 
 cd -
